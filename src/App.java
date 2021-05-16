195c106cc9354bb19126ab4ed263a76e202f0a34
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-05-16 23:08:54 +0700

    refactor(package): rename package views to Views

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import views.Dashboard;
+import Views.Dashboard;
 /**
  *
  * @author Alfian Andi Nugraha
--- src/Views/Dashboard.java
+++ src/Views/Dashboard.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package views;
+package Views;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.JOptionPane;
