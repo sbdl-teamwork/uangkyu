ac865a4b987d56843462804d08a9849ff5ba1862
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-05-18 23:14:01 +0700

    feat(model): add model for Type table

---
+++ src/Models/Type.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author alfian-andi
+ */
+public class Type {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public Type setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
