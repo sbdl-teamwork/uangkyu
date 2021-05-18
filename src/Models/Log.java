c57c33759b5ada563ecf8d979e755ca5a66b4132
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-05-18 19:36:43 +0700

    feat(log): add property for Log class

--- src/Models/Log.java
+++ src/Models/Log.java
@@ -10,5 +10,44 @@ package Models;
  * @author alfian-andi
  */
 public class Log {
-    
+    private int id;
+    private User user;
+    private String message;
+    private String created_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public Log setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Log setUser(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Log setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public String getCreatedAt() {
+        return created_at;
+    }
+
+    public Log setCreatedAt(String created_at) {
+        this.created_at = created_at;
+        return this;
+    }
 }
