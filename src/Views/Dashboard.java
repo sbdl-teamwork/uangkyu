0d29e8219de6dbbbdb68a8eada2dddbc09138a7b
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-05-18 11:55:21 +0700

    fix(views): centering Dashboard on screen

--- src/Views/Dashboard.java
+++ src/Views/Dashboard.java
@@ -526,6 +526,7 @@ public class Dashboard extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void inputDescriptionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputDescriptionActionPerformed
