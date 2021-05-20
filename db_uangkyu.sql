5d7e81c28d7ff3e68ebeb4aaa3f44bdadd21bca8
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-05-20 10:18:05 +0700

    feat(sql): update query sql

--- db_uangkyu.sql
+++ db_uangkyu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Bulan Mei 2021 pada 11.20
+-- Waktu pembuatan: 20 Bulan Mei 2021 pada 05.17
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.5
 
@@ -23,13 +23,79 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `db_uangkyu` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `db_uangkyu`;
 
+DELIMITER $$
+--
+-- Prosedur
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_activity` (IN `id_param` INT, IN `user_id_param` INT)  BEGIN
+	DELETE FROM activities WHERE id = id_param AND user_id = user_id_param;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_expense_activity` (IN `description` VARCHAR(250), IN `nominal` FLOAT, IN `user_id` INT, IN `date_at` DATETIME)  BEGIN
+	INSERT INTO activities 
+    	(description, nominal, user_id, date_at, type) 
+    VALUES 
+    	(description, nominal, user_id, date_at,'PLRN');
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_income_activity` (IN `description` VARCHAR(250), IN `nominal` FLOAT, IN `user_id` INT, IN `date_at` DATETIME)  BEGIN
+	INSERT INTO activities 
+    	(description, nominal, user_id, date_at, type) 
+    VALUES 
+    	(description, nominal, user_id, date_at,'PMSK');
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `register_user` (`name` VARCHAR(250), `email` VARCHAR(250), `password` VARCHAR(250))  BEGIN
+    INSERT INTO users (name, email, password) VALUES (name, email, password);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_expense_activity` (IN `id_param` INT, IN `description_param` VARCHAR(250), IN `nominal_param` FLOAT, IN `user_id_param` INT, IN `date_at_param` DATETIME)  BEGIN
+	UPDATE 
+    	activities 
+    SET 
+    	description = description_param, 
+        nominal = nominal_param, 
+        date_at = date_at_param,
+        type = 'PLRN'
+    WHERE 
+    	id = id_param AND user_id = user_id_param;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_income_activity` (IN `id_param` INT, IN `description_param` VARCHAR(250), IN `nominal_param` FLOAT, IN `user_id_param` INT, IN `date_at_param` DATETIME)  BEGIN
+	UPDATE 
+    	activities 
+    SET 
+    	description = description_param, 
+        nominal = nominal_param, 
+        date_at = date_at_param,
+        type = 'PMSK'
+    WHERE 
+    	id = id_param AND user_id = user_id_param;
+END$$
+
+--
+-- Fungsi
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_total_expense` (`user_id_param` INT, `from_param` DATETIME, `to_param` DATETIME) RETURNS FLOAT BEGIN
+  DECLARE total FLOAT;
+  SELECT SUM(nominal) INTO total FROM activities WHERE user_id = user_id_param AND type = 'PLRN' AND date_at >= from_param AND date_at <= to_param;
+  RETURN total;
+END$$
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `get_total_income` (`user_id_param` INT, `from_param` DATETIME, `to_param` DATETIME) RETURNS FLOAT BEGIN
+  DECLARE total FLOAT;
+  SELECT SUM(nominal) AS total INTO total FROM activities WHERE user_id = user_id_param AND type = 'PMSK' AND date_at >= from_param AND date_at <= to_param;
+  RETURN total;
+END$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `activities`
 --
 
-DROP TABLE IF EXISTS `activities`;
 CREATE TABLE `activities` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -44,7 +110,6 @@ CREATE TABLE `activities` (
 --
 -- Trigger `activities`
 --
-DROP TRIGGER IF EXISTS `activity_update_at`;
 DELIMITER $$
 CREATE TRIGGER `activity_update_at` BEFORE UPDATE ON `activities` FOR EACH ROW BEGIN
     SET NEW.updated_at = NOW();
@@ -58,7 +123,6 @@ DELIMITER ;
 -- Stand-in struktur untuk tampilan `transaction_activities`
 -- (Lihat di bawah untuk tampilan aktual)
 --
-DROP VIEW IF EXISTS `transaction_activities`;
 CREATE TABLE `transaction_activities` (
 `id` int(11)
 ,`user_id` int(11)
@@ -78,7 +142,6 @@ CREATE TABLE `transaction_activities` (
 -- Struktur dari tabel `types`
 --
 
-DROP TABLE IF EXISTS `types`;
 CREATE TABLE `types` (
   `id` varchar(250) NOT NULL,
   `name` varchar(250) NOT NULL
@@ -90,7 +153,6 @@ CREATE TABLE `types` (
 -- Struktur dari tabel `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(250) NOT NULL,
@@ -99,17 +161,6 @@ CREATE TABLE `users` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Trigger `users`
---
-DROP TRIGGER IF EXISTS `register_user_log`;
-DELIMITER $$
-CREATE TRIGGER `register_user_log` AFTER INSERT ON `users` FOR EACH ROW BEGIN
-    INSERT INTO logs (user_id, message) VALUES (NEW.id, "Berhasil membuat akun");
-END
-$$
-DELIMITER ;
-
 -- --------------------------------------------------------
 
 --
@@ -117,7 +168,6 @@ DELIMITER ;
 --
 DROP TABLE IF EXISTS `transaction_activities`;
 
-DROP VIEW IF EXISTS `transaction_activities`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `transaction_activities`  AS  select `activities`.`id` AS `id`,`activities`.`user_id` AS `user_id`,`activities`.`description` AS `description`,`activities`.`nominal` AS `nominal`,`activities`.`type` AS `type`,`activities`.`date_at` AS `date_at`,`activities`.`created_at` AS `created_at`,`activities`.`updated_at` AS `updated_at`,`types`.`id` AS `type_id`,`types`.`name` AS `name` from (`activities` join `types` on(`types`.`id` = `activities`.`type`)) ;
 
 --
